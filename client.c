162110109250cef6dc3c0d423bb2b30d1fe7e200
Yumna Alkhidir <yalkhidi@lab1r2s9.42abudhabi.ae>
2025-04-30 22:08:17 +0400

    minitalk

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: yalkhidi <yalkhidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:43:48 by yalkhidi          #+#    #+#             */
-/*   Updated: 2025/04/30 18:46:24 by yalkhidi         ###   ########.fr       */
+/*   Updated: 2025/04/30 22:04:01 by yalkhidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int ac, char **av)
 	{
 		pid = ft_atoi(av[1]);
 		message = av[2];
+		ft_printf("pid: %d\n", pid);
+		ft_printf("message: %s\n", message);
 		kill(pid, SIGINT);
 	}
 	else
