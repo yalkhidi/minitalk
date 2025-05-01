aee1aa2012376f96c684ed301785b25be0521483
Yumna Alkhidir <yalkhidi@lab1r2s12.42abudhabi.ae>
2025-05-01 18:14:16 +0400

    minitalk

--- client.c
+++ client.c
@@ -6,16 +6,45 @@
 /*   By: yalkhidi <yalkhidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:43:48 by yalkhidi          #+#    #+#             */
-/*   Updated: 2025/04/30 22:04:01 by yalkhidi         ###   ########.fr       */
+/*   Updated: 2025/05/01 17:52:44 by yalkhidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_message(pid_t pid, char letter)
+{
+	int		j;
+	char	let[9];
+
+	let[8] = '\0';
+	j = 7;
+	while (j >= 0)
+	{
+		if (letter % 2 == 1)
+		{
+			let[j] = '1';
+			kill(pid, SIGUSR1);
+		}
+		else if (letter % 2 == 0)
+		{
+			let[j] = '0';
+			kill(pid, SIGUSR2);
+		}
+		letter = letter / 2;
+		
+		j--;
+	}
+	ft_printf("%s\n", let);
+}
+
+
+
 int	main(int ac, char **av)
 {
 	pid_t	pid;
 	char	*message;
+	int		i;
 
 	if (ac == 3)
 	{
@@ -23,7 +52,12 @@ int	main(int ac, char **av)
 		message = av[2];
 		ft_printf("pid: %d\n", pid);
 		ft_printf("message: %s\n", message);
-		kill(pid, SIGINT);
+		i = 0;
+		while (message[i])
+		{
+			send_message(pid, message[i]);
+			i++;
+		}
 	}
 	else
 		ft_printf("wrong format\n");
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: yalkhidi <yalkhidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:45:01 by yalkhidi          #+#    #+#             */
-/*   Updated: 2025/04/30 22:06:28 by yalkhidi         ###   ########.fr       */
+/*   Updated: 2025/05/01 17:55:14 by yalkhidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,62 @@ void	handle_in(int sig)
 	exit(0);
 }
 
-// int	main(void)
+// void	handle_1(int sig)
 // {
-// 	pid_t	pid;
-
-// 	pid = getpid();
-// 	ft_printf("%d\n", pid);
-// 	signal(SIGINT, handle_in);
-// 	while (1)
-// 		ft_printf("");
+// 	ft_printf("sig: %d\n", sig);
+// 	ft_printf("SIGUSR1\n");
+// 	exit(1);
 // }
 
-void	handle_1(int sig)
+// void	handle_2(int sig)
+// {
+// 	ft_printf("sig: %d\n", sig);
+// 	ft_printf("SIGUSR2\n");
+// 	exit(1);
+// }
+
+void	receive_message(int sig)
 {
-	ft_printf("sig: %d\n", sig);
-	ft_printf("SIGUSR1\n");
-	exit(1);
+	static int	bits[8];
+	static int	bit_index = 0;
+	int			i;
+	char		letter;
+
+	if (sig == SIGUSR1)
+	{
+		bits[bit_index] = 1;
+		printf("received 1\n");
+	}
+	else if(sig == SIGUSR2)
+	{
+		bits[bit_index] = 0;
+		printf("received 0\n");
+	}
+	bit_index++;
+	if(bit_index == 8)
+	{
+		letter = 0;
+		i = 0;
+		while(i < 8)
+		{
+			letter = letter * 2 + bits[i];
+			i++;
+		}
+		ft_printf("%c", letter);
+		bit_index = 0;
+	}
 }
 
-void	handle_2(int sig)
-{
-	ft_printf("sig: %d\n", sig);
-	ft_printf("SIGUSR2\n");
-	exit(1);
-}
+
 
 int	main(void)
 {
-	signal(SIGUSR1, handle_1);
-	signal(SIGUSR2, handle_2);
+	// signal(SIGUSR1, handle_1);
+	// signal(SIGUSR2, handle_2);
 	signal(SIGINT, handle_in);
-	ft_printf("pid: %d", getpid());
+	signal(SIGUSR1, receive_message);
+	signal(SIGUSR2, receive_message);
+	ft_printf("pid: %d\n", getpid());
 	while (1)
 		pause();
 }
